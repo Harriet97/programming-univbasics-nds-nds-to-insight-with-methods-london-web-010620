269219593fadbb4bc2ca437b258ef5919abaae70
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-12 15:00:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,23 +1,23 @@
 require 'directors_database'
 
-def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
+def directors_totals(nds)
+#this take the director and the gross_for_director to make a hash
+gross_totals = {}
+counter = 0 
+while counter < nds.length do 
+  gross_totals[nds[counter][:name]] = gross_for_director(nds[counter])
+  counter += 1 
+end 
+gross_totals
 end
+    
 
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-  total
+def gross_for_director(director_data)
+worldwide_gross_total = 0 
+counter = 0 
+  while counter < director_data[:movies].length do 
+    worldwide_gross_total += director_data[:movies][counter][:worldwide_gross]
+    counter += 1 
+  end 
+  worldwide_gross_total #this makes thhe totals for each director
 end
